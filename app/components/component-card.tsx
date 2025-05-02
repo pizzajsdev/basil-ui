d51ab28296643c6bfc50aaa112bf71c17d148cec
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2025-05-02 12:30:46 +0200

    improve site layout

--- app/components/component-card.tsx
+++ app/components/component-card.tsx
@@ -15,7 +15,7 @@ export function ComponentCard({
   return (
     <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[250px] relative">
       <div className="flex items-center justify-between gap-2 flex-row">
-        <h2 className="text-sm text-muted-foreground sm:pl-3">{description}</h2>
+        <h3 className="text-sm text-muted-foreground sm:pl-3">{description}</h3>
         <div className="flex items-center gap-2">
           {openInV0 && <OpenInV0Button name={name} className="w-fit" />}
           <InstallButton name={name} />
